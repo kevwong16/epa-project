2c6678b2197d85ccad4710c2c39c0d52d1d864c4
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-11-11 12:28:54 +0000

    Update lemp-setup.sh
    Removed comments as was causing issues with script

--- lemp-setup.sh
+++ lemp-setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo apt -y update
 sudo apt -y upgrade
-sudo touch /root/testing.txt # this file will contain the output of our LEMP stack unit tests
+sudo touch /root/testing.txt
 sudo apt -y install nginx
-sudo systemctl start nginx && sudo systemctl enable nginx # this starts and enables nginx on a server reboot. The 2nd command will only run if the first command is successful
+sudo systemctl start nginx && sudo systemctl enable nginx
 sudo systemctl status nginx > /root/testing.txt
 sudo apt -y install mariadb-server
 sudo systemctl start mariadb && sudo systemctl enable mariadb
