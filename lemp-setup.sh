3235e6506d024912c8897aead544c84cf8c3be61
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-11-11 13:15:40 +0000

    Update lemp-setup.sh
    Renamed apache testing page

--- lemp-setup.sh
+++ lemp-setup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 sudo apt -y update
 sudo apt -y upgrade
-sudo touch /root/testing.txt
+sudo touch /root/testing.txt # this file will contain the output of our LEMP stack unit tests
 sudo apt -y install nginx
-sudo systemctl start nginx && sudo systemctl enable nginx
+sudo systemctl start nginx && sudo systemctl enable nginx # this starts and enables nginx on a server reboot. The 2nd command will only run if the first command is successful
 sudo systemctl status nginx > /root/testing.txt
 sudo apt -y install mariadb-server
 sudo systemctl start mariadb && sudo systemctl enable mariadb
-sudo systemctl status mariadb >> /root/testing.txt
+systemctl status mariadb >> /root/testing.txt
 sudo apt -y install php php-cli php-common php-imap php-fpm php-snmp php-xml php-zip php-mbstring php-curl php-mysqli php-gd php-intl
 sudo php -v >> /root/testing.txt
+sudo mv /var/www/html/index.html /var/www/html/index.html.old # rename apache testing page
