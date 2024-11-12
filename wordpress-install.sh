f924e30388fdae237b168a40c601b6ca6193d20a
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-11-12 15:58:59 +0000

    Update wordpress-install.sh
    added final database bash automation

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-sudo cd /var/www/html
+sudo cd /var/www/
+sudo rm -rf html
 sudo apt -y install unzip
 sudo wget https://wordpress.org/latest.zip
 sudo unzip latest.zip
 sudo rm latest.zip
-sudo mariadb -u root
-# create database wordpress;
-# create user wpuser@localhost identified by 'theclassof92';
-# grant all privileges on wordpress.* to wpuser@localhost;
-# flush privileges;
-# exit;
-# wget wp-config.php from s3bucket
+sudo mv wordpress html
+# sudo mariadb -u root
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS wordpress"
+sudo mysql -e "CREATE USER wpuser@localhost identified by 'theclassof92'"
+sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* to wpuser@localhost"
+sudo mysql -e "FLUSH PRIVILEGES"
+sudo cd /var/www/html/wordpress
+sudo wget https://kevin-epa-bucket.s3.us-east-1.amazonaws.com/wp-config.php
 sudo chmod 640 wp-config.php
-sudo chown -R www-data:www-data wordpress/
-sudo cd /etc/nginx/conf.d/
+sudo chown -R www-data:www-data /var/www/html/wordpress
+# sudo cd /etc/nginx/conf.d/
 # sudo touch wordpress.conf pull from s3bucket
