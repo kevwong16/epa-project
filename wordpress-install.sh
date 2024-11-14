a78fd7d817f3c10eea8ffec1111501068f4affc7
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-11-14 11:13:33 +0000

    Update wordpress-install.sh
    Added full file paths

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
-sudo cd /var/www/
-sudo rm -rf html
+sudo rm -rf /var/www/html
 sudo apt -y install unzip
-sudo wget https://wordpress.org/latest.zip
-sudo unzip latest.zip
-sudo rm latest.zip
-sudo mv wordpress html
+sudo wget -O /var/www/latest.zip https://wordpress.org/latest.zip
+sudo unzip /var/www/latest.zip
+sudo rm /var/www/latest.zip
+sudo mv /var/www/wordpress /var/www/html 
 # sudo mariadb -u root
 sudo mysql -e "CREATE DATABASE IF NOT EXISTS wordpress"
-sudo mysql -e "CREATE USER wpuser@localhost identified by 'theclassof92'"
+sudo mysql -e "CREATE USER IF NOT EXISTS wpuser@localhost identified by 'theclassof92'"
 sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* to wpuser@localhost"
 sudo mysql -e "FLUSH PRIVILEGES"
-sudo cd /var/www/html/wordpress
-sudo wget https://kevin-epa-bucket.s3.us-east-1.amazonaws.com/wp-config.php
-sudo chmod 640 wp-config.php
-sudo chown -R www-data:www-data /var/www/html/wordpress
+sudo wget -O /var/www/html/wp-config.php https://kevin-epa-bucket.s3.us-east-1.amazonaws.com/wp-config.php
+sudo chmod 640 /var/www/html/wp-config.php 
+sudo chown -R www-data:www-data /var/www/html/
 # sudo cd /etc/nginx/conf.d/
 # sudo touch wordpress.conf pull from s3bucket
