0b1f96c092a226e34ba069c54eda6a33ceae32cd
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-11-14 09:54:19 +0000

    Update git-setup.sh
    Updated git clone to /root

--- git-setup.sh
+++ git-setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo apt -y install git
-sudo cd /root/
-sudo git clone https://github.com/kevwong16/epa-project.git
+sudo git clone https://github.com/kevwong16/epa-project.git /root
 sudo chmod -R 755 epa-project
 sudo bash epa-project/lemp-setup.sh
