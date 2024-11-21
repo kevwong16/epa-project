5c7b78c6396dba61d6099786cb7bea9b95694049
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-11-21 15:53:06 +0000

    Update git-setup.sh
    This version will check the exit status of each command and print a message indicating whether the command succeeded or failed. Additionally, it will exit the script if any command fails.

--- git-setup.sh
+++ git-setup.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
-# sudo apt -y install git
+
+# Function to check the exit status of the last executed command
+check_exit_status() {
+    if [ $? -ne 0 ]; then
+        echo "Error: $1 failed."
+        exit 1
+    else
+        echo "$1 succeeded."
+    fi
+}
+
+# Update package lists
 sudo apt -y update
+check_exit_status "apt update"
+
+# Upgrade installed packages
 sudo apt -y upgrade
+check_exit_status "apt upgrade"
+
+# Clone the GitHub repository
 sudo git clone https://github.com/kevwong16/epa-project.git /root/epa-project
+check_exit_status "git clone"
+
+# Change permissions of the cloned repository
 sudo chmod -R 755 /root/epa-project
+check_exit_status "chmod"
+
+# Run the setup script
 sudo bash /root/epa-project/lemp-setup.sh
+check_exit_status "lemp-setup.sh script"
