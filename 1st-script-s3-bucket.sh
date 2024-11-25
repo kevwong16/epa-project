00cad0ef198f6c20fbc3beecb419ba939a28842e
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-11-25 14:57:45 +0000

    Update 1st-script-s3-bucket.sh
    Tidied up code and added comments explaining what each line of code is doing

--- 1st-script-s3-bucket.sh
+++ 1st-script-s3-bucket.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-sudo wget /root/git-setup.sh https://kevin-epa-bucket.s3.us-east-1.amazonaws.com/git-setup.sh
-sudo chmod 755 git-setup.sh
-sudo bash git-setup.sh
+
+# Download the `git-setup.sh` script from the specified S3 bucket to the `/root/` directory
+sudo wget -O /root/git-setup.sh https://kevin-epa-bucket.s3.us-east-1.amazonaws.com/git-setup.sh
+
+# Set executable permissions for the downloaded `git-setup.sh` script
+sudo chmod 755 /root/git-setup.sh
+
+# Execute the `git-setup.sh` script
+sudo bash /root/git-setup.sh
