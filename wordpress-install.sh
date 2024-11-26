7c42228e6246c6f45cfb6e005cf93e22d964c3ba
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-11-26 15:13:43 +0000

    Update wordpress-install.sh
    added lines to generate random username and SALT values

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -20,21 +20,27 @@ sudo mv /var/www/wordpress /var/www/html
 
 # Generate a random password for the WordPress database user
 password=$(tr -dc 'A-Za-z0-9!' < /dev/urandom | head -c 25)
+username=$(tr -dc 'A-Za-z' < /dev/urandom | head -c 25)
+
+echo $password > creds.txt
+echo $username >> creds.txt
 
 # Create the WordPress database if it doesn't already exist
-sudo mysql -e "CREATE DATABASE IF NOT EXISTS wordpress"
+sudo mysql -e "CREATE DATABASE IF NOT EXISTS $username"
 
 # Create a database user `wpuser` with the generated password, if not already existing
-sudo mysql -e "CREATE USER IF NOT EXISTS wpuser@localhost IDENTIFIED BY '$password'"
+sudo mysql -e "CREATE USER IF NOT EXISTS $username@localhost IDENTIFIED BY '$password'"
 
 # Grant all privileges on the `wordpress` database to the new user
-sudo mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO wpuser@localhost"
+sudo mysql -e "GRANT ALL PRIVILEGES ON $username.* TO $username@localhost"
 
 # Apply the changes made to the database privileges
 sudo mysql -e "FLUSH PRIVILEGES"
 
 # Download a pre-configured `wp-config.php` file from the specified S3 bucket
-sudo wget -O /var/www/html/wp-config.php https://kevin-epa-bucket.s3.us-east-1.amazonaws.com/wp-config.php
+# sudo wget -O /var/www/html/wp-config.php https://kevin-epa-bucket.s3.us-east-1.amazonaws.com/wp-config.php
+
+sudo mv /var/www/html/wp-config-sample.php /var/www/html/wp-config.php
 
 # Secure the `wp-config.php` file by setting appropriate permissions
 sudo chmod 640 /var/www/html/wp-config.php
@@ -42,13 +48,15 @@ sudo chmod 640 /var/www/html/wp-config.php
 # Set ownership of the entire WordPress directory to the `www-data` user and group
 sudo chown -R www-data:www-data /var/www/html/
 
-# Replace the placeholder `password_here` in the `wp-config.php` file with the generated database password
+# Replace the placeholder 'password_here' in wp-config.php with the generated password.
 sed -i "s/password_here/$password/g" /var/www/html/wp-config.php
+sed -i "s/username_here/$username/g" /var/www/html/wp-config.php
+sed -i "s/database_name_here/$username/g" /var/www/html/wp-config.php
 
-# Placeholder for additional Nginx configuration (commented out in this script)
-# Navigate to the Nginx configuration directory
+SALT=$(curl -L https://api.wordpress.org/secret-key/1.1/salt/)
+STRING='put your unique phrase here'
+printf '%s\n' "g/$STRING/d" a "$SALT" . w | ed -s /var/www/html/wp-config.php
+
+# (Optional) Navigate to the /etc/nginx/conf.d/ directory and create a new Nginx configuration file for WordPress.
 # sudo cd /etc/nginx/conf.d/
-
-# Create a new configuration file for WordPress by downloading it from the S3 bucket
-# sudo touch wordpress.conf
-# sudo wget -O wordpress.conf https://kevin-epa-bucket.s3.us-east-1.amazonaws.com/wordpress.conf
+# sudo touch wordpress.conf pull from s3bucket
