42c4e874ddbe427cd57230a4c49ddacc736b15af
Kevin Wong <133127934+kevwong16@users.noreply.github.com>
2024-12-17 16:12:35 +0000

    Update wordpress-install.sh
    updated variables for rds username and password

--- wordpress-install.sh
+++ wordpress-install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Constants
+aws_rds_endpoint=AWS_RDS_ENDPOINT # Replace with your RDS endpoint
+rds_username=RDS_USER   # Replace with your RDS admin username
+rds_password=RDS_PASSWORD   # Replace with your RDS admin password
+
+
 # Remove the existing web root directory (force delete)
 sudo rm -rf /var/www/html
 
@@ -19,23 +25,23 @@ sudo rm /var/www/latest.zip
 sudo mv /var/www/wordpress /var/www/html
 
 # Generate a random password for the WordPress database user
-password=$(tr -dc 'A-Za-z0-9!' < /dev/urandom | head -c 25)
-username=$(tr -dc 'A-Za-z' < /dev/urandom | head -c 25)
+# password=$(tr -dc 'A-Za-z0-9!' < /dev/urandom | head -c 25)
+# username=$(tr -dc 'A-Za-z' < /dev/urandom | head -c 25)
 
-echo $password > creds.txt
-echo $username >> creds.txt
+# echo $password > creds.txt
+# echo $username >> creds.txt
 
 # Create the WordPress database if it doesn't already exist
-sudo mysql -e "CREATE DATABASE IF NOT EXISTS $username"
+# sudo mysql -e "CREATE DATABASE IF NOT EXISTS $username"
 
 # Create a database user `wpuser` with the generated password, if not already existing
-sudo mysql -e "CREATE USER IF NOT EXISTS $username@localhost IDENTIFIED BY '$password'"
+# sudo mysql -e "CREATE USER IF NOT EXISTS $username@localhost IDENTIFIED BY '$password'"
 
 # Grant all privileges on the `wordpress` database to the new user
-sudo mysql -e "GRANT ALL PRIVILEGES ON $username.* TO $username@localhost"
+# sudo mysql -e "GRANT ALL PRIVILEGES ON $username.* TO $username@localhost"
 
 # Apply the changes made to the database privileges
-sudo mysql -e "FLUSH PRIVILEGES"
+# sudo mysql -e "FLUSH PRIVILEGES"
 
 # Download a pre-configured `wp-config.php` file from the specified S3 bucket
 # sudo wget -O /var/www/html/wp-config.php https://kevin-epa-bucket.s3.us-east-1.amazonaws.com/wp-config.php
@@ -49,9 +55,11 @@ sudo chmod 640 /var/www/html/wp-config.php
 sudo chown -R www-data:www-data /var/www/html/
 
 # Replace the placeholder 'password_here' in wp-config.php with the generated password.
-sed -i "s/password_here/$password/g" /var/www/html/wp-config.php
-sed -i "s/username_here/$username/g" /var/www/html/wp-config.php
-sed -i "s/database_name_here/$username/g" /var/www/html/wp-config.php
+sed -i "s/password_here/$rds_password/g" /var/www/html/wp-config.php
+sed -i "s/username_here/$rds_username/g" /var/www/html/wp-config.php
+sed -i "s/database_name_here/$rds_username/g" /var/www/html/wp-config.php
+sed -i "s/localhost/$aws_rds_endpoint/g" /var/www/html/wp-config.php
+
 
 SALT=$(curl -L https://api.wordpress.org/secret-key/1.1/salt/)
 STRING='put your unique phrase here'
